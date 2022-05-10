85d2dd2eafaea226b50983a6ed5f238c61122b8b
Robiul Hasan <muhammadrobiul502@gmail.com>
2022-05-10 16:13:29 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#0FCFEC",
+          secondary: "#19D3AE",
+          accent: "#37cdbe",
+          neutral: "#3A4256",
+          "base-100": "#ffffff",
+        },
+      },
+      // "dark",
+      // "cupcake",
+    ],
+  },
+  plugins: [require("daisyui")],
+}
