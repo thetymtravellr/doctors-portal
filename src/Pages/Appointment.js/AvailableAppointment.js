f82a5646f8850988be8a44ac59a72045e25ecc31
Robiul Hasan <muhammadrobiul502@gmail.com>
2022-05-17 16:31:10 +0600

    code

--- src/Pages/Appointment.js/AvailableAppointment.js
+++ src/Pages/Appointment.js/AvailableAppointment.js
@@ -1,40 +1,47 @@
-import { format } from 'date-fns';
-import React, { useEffect, useState } from 'react';
-import BookingModal from './BookingModal';
-import Service from './Service';
+import { format } from "date-fns";
+import React, { useState } from "react";
+import { useQuery } from "react-query";
+import Loading from "../Shared/Loading";
+import BookingModal from "./BookingModal";
+import Service from "./Service";
 
-const AvailableAppointment = ({date}) => {
-    const [services, setServices] = useState([]);
-    const [treatment, setTreatment] = useState(null)
+const AvailableAppointment = ({ date }) => {
+  // const [services, setServices] = useState([]);
+  const [treatment, setTreatment] = useState(null);
+  const formattedDate = format(date, "PP");
+  const { data: services, isLoading } = useQuery("available", () =>
+    fetch(`http://localhost:5000/available?date=${formattedDate}`).then((res) =>
+      res.json()
+    )
+  );
 
-    useEffect(()=>{
-        fetch('http://localhost:5000/services')
-        .then(res => res.json())
-        .then(data => setServices(data))
-    },[])
+  if (isLoading) {
+    return <Loading></Loading>;
+  }
 
-    return (
-        <section>
-            <h4 className='text-xl text-secondary text-center'>Available Appointment on {format(date, 'PP')}</h4>
-            <div className='grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-6'>
-                {
-                    services.map(service => <Service
-                    service={service}
-                    key={service._id}
-                    setTreatment={setTreatment}
-                    >
-                    </Service>)
-                }
-            </div>
-            {
-                treatment && <BookingModal
-                treatment={treatment}
-                setTreatment={setTreatment}
-                date={date}
-                ></BookingModal>
-            }
-        </section>
-    );
+  return (
+    <section>
+      <h4 className="text-xl text-secondary text-center">
+        Available Appointment on {format(date, "PP")}
+      </h4>
+      <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-6">
+        {services?.map((service) => (
+          <Service
+            service={service}
+            key={service._id}
+            setTreatment={setTreatment}
+          ></Service>
+        ))}
+      </div>
+      {treatment && (
+        <BookingModal
+          treatment={treatment}
+          setTreatment={setTreatment}
+          date={date}
+        ></BookingModal>
+      )}
+    </section>
+  );
 };
 
 export default AvailableAppointment;
